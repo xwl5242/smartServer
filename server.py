67b5c42093ce3295e225135521180e1208165fe5
LAPTOP-HHIRL5CI\86153 <xwl5242@163.com>
2022-01-25 21:50:21 +0800

    update

--- server.py
+++ server.py
@@ -3,14 +3,18 @@ import os
 import json
 import base64
 import requests
-import threading
 from tools.Utils import Utils
-from urllib.parse import unquote
+from config.Config import Conf
+from urllib.parse import unquote, quote
 from tools.IPTV import HqTV, IpTV, HdTV, CCTV, WS_TV, GAT_TV, Service
 from flask import Flask, jsonify, request, Response, render_template
 
 
+aes_iv = '0102891350607087'
+aes_key = '0CoJUm6poC8W8jud'
 M3U8 = os.path.join(os.path.dirname(__file__), 'm3u8')
+M3U8_URL = 'http://localhost:5000/iptv/m3u8?m3u8=' \
+    if Conf.DB_PT == 'test' else 'https://smart.quanchonger.com/iptv/iptv/m3u8?m3u8='
 
 
 def substr(string, start, end):
@@ -61,24 +65,27 @@ def video_player(video_id, channel):
     channel = channel if channel else 1
     video_url = HqTV().get_video_url(video_id) if int(channel) == 1 else \
         (IpTV.get_video_url(video_id) if int(channel) == 2 else HdTV.get_video_url(video_id))
-    return jsonify({"url": video_url})
+    return jsonify({"url": base64.b64encode(Utils.aes_encrypt(aes_key, aes_iv, video_url).encode()).decode('utf-8')})
 
 
-@app.route('/iptv/player')
-def player():
-    return render_template("1.html")
+@app.route('/iptv/player/<channel>')
+def player(channel):
+    params = base64.b64decode(request.args['_u']).decode('utf-8')
+    player_url = Utils.aes_decrypt(aes_key, aes_iv, str(params))
+    if int(channel) == 2:
+        player_url = M3U8_URL + base64.b64encode(quote(player_url).encode()).decode("utf-8")
+    return render_template("player.html", player=player_url)
 
 
 @app.route('/iptv/m3u8')
 def video_m3u8():
-    # aHR0cCUzQSUyRiUyRjExMS40MC4xOTYuMjklMkZQTFRWJTJGODg4ODg4ODglMkYyMjQlMkYzMjIxMjI1NzY5JTJGaW5kZXgubTN1OA==
     url = request.args['m3u8']
     if url:
         url = unquote(base64.b64decode(url).decode("utf-8"))
         resp = requests.get(url, headers=Utils.ua())
-        if resp and resp.text:
-            url_list = [u.replace("\r", "") for u in resp.text.split('\n') if 'hls.ts' in u]
-            threading.Thread(target=ts_download, args=(url_list,))
+        # if resp and resp.text:
+        #     url_list = [u.replace("\r", "") for u in resp.text.split('\n') if 'hls.ts' in u]
+        #     threading.Thread(target=ts_download, args=(url_list,))
         if resp and resp.content:
             resp = Response(resp.content, content_type='application/x-mpegURL')
             return resp
@@ -87,15 +94,18 @@ def video_m3u8():
 
 @app.before_request
 def video_hls_ts():
-    if '.ts' in request.path:
+    if '.hls.ts' in request.path:
         url = request.path
         url = url[url.rfind('/'):]
-#         http://111.40.196.29/PLTV/88888888/224/3221225769/
-        resp = requests.get('http://111.40.196.29/PLTV/88888888/224/3221225769'+url, headers=Utils.ua())
+        referer = request.headers['Referer']
+        referer = referer[referer.find('=')+1:]
+        referer = base64.b64decode(referer).decode('utf-8')
+        player_url = Utils.aes_decrypt(aes_key, aes_iv, referer)
+        player_url = player_url[0: player_url.rfind('/')]
+        resp = requests.get(player_url+url, headers=Utils.ua())
         if resp and resp.content:
             resp = Response(resp.content, content_type='application/x-mpegURL')
             return resp
-        # return redirect('http://111.40.196.29/PLTV/88888888/224/3221225769'+url)
 
 
 def ts_download(url_list):
